b501d5cf3a1f9c0dc7f5d8d102f1da9e28518831
Patryk Rakowski <patrykr2001@gmail.com>
2026-03-21 11:47:03 +0100

    add user registration form and user model with validation

---
+++ Lab02/awir2/src/main/java/edu/zut/awir/awir2/controller/UserController.java
@@ -0,0 +1,43 @@
+package edu.zut.awir.awir2.controller;
+
+import edu.zut.awir.awir2.model.User;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/new")
+    public String showForm(Model model) {
+        if (!model.containsAttribute("user")) {
+            model.addAttribute("user", new User());
+        }
+        return "add-user";
+    }
+
+    @PostMapping
+    public String processForm(@Valid @ModelAttribute("user") User user,
+                              BindingResult result,
+                              RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user",
+                    result);
+            redirectAttributes.addFlashAttribute("user", user);
+            return "redirect:/users/new";
+        }
+        redirectAttributes.addFlashAttribute("user", user);
+        return "redirect:/users/info";
+    }
+
+    @GetMapping("/info")
+    public String showInfo() {
+        return "user-info";
+    }
+}
---
+++ Lab02/awir2/src/main/java/edu/zut/awir/awir2/model/User.java
@@ -0,0 +1,20 @@
+package edu.zut.awir.awir2.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Min(1)
+    private long id;
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+}
