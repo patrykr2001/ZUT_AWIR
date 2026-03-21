5f02acd033a1f7783991fcb224cb28b9fd72021e
Patryk Rakowski <patrykr2001@gmail.com>
2026-03-21 11:37:11 +0100

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ Lab02/awir2/src/main/java/edu/zut/awir/awir2/Awir2Application.java
@@ -0,0 +1,13 @@
+package edu.zut.awir.awir2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awir2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Awir2Application.class, args);
+    }
+
+}
