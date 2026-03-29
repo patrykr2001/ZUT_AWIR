dce9fe6a46111754de6374951f46b0e3bbb2ae30
Patryk Rakowski <patrykr2001@gmail.com>
2026-03-29 10:06:18 +0200

    initialize user registration feature with form, validation, and controller

---
+++ Lab03/awir3/src/main/java/edu/zut/awir/awir3/Awir3Application.java
@@ -0,0 +1,13 @@
+package edu.zut.awir.awir3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awir3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Awir3Application.class, args);
+    }
+
+}
---
+++ Lab03/awir3/src/test/java/edu/zut/awir/awir3/Awir3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zut.awir.awir2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awir2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
