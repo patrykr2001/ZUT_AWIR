2b37e76f9430d17d4d96e1d1896790641dbfd43b
Patryk Rakowski <patrykr2001@gmail.com>
2026-04-11 12:32:14 +0200

    add MockitoBean for UserService in UserRestControllerTest

--- Lab04/awir4/src/test/java/edu/zut/awir/awir4/web/rest/UserRestControllerTest.java
+++ Lab04/awir4/src/test/java/edu/zut/awir/awir4/web/rest/UserRestControllerTest.java
@@ -2,10 +2,12 @@ package edu.zut.awir.awir4.web.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.zut.awir.awir4.model.User;
+import edu.zut.awir.awir4.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,6 +19,9 @@ class UserRestControllerTest {
     MockMvc mvc;
     @Autowired
     ObjectMapper om;
+
+    @MockitoBean
+    UserService userService;
 
     @Test
     void rejectsInvalidPayload() throws Exception {
